710e2d830e736fefa4d398db897b0e0a90307b3b
andyngojs <dongngo.2000@gmail.com>
2023-02-11 15:39:42 +0700

    fix a little bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import {RootNavigator} from '@src/navigation';
 
 function App() {
   return <RootNavigator />;
 }
-
-const styles = StyleSheet.create({});
 
 export default App;
--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,7 +1,6 @@
-import {useDispatch} from 'react-redux';
-import {useSelector} from 'react-redux/es/exports';
-import {TypedUseSelectorHook} from 'react-redux/es/types';
-import {RootState, AppDispatch} from './store';
+import {useDispatch, useSelector} from 'react-redux';
+import type {TypedUseSelectorHook} from 'react-redux';
+import type {RootState, AppDispatch} from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
